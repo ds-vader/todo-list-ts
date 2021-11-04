1a03af1178d8c46845c8b40b22b4774b41f94cb0
Dmytro Medynskyi <48735893+twoSad@users.noreply.github.com>
2021-11-04 15:02:32 +0200

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import React, { FC, ChangeEvent, useState } from 'react';
+import './App.css';
+import TodoTask from './TodoTask';
+import { ITask } from './Interfaces';
+
+const App: FC = () => {
+  const [task, setTask] = useState<string>('');
+  const [todoList, setTodoList] = useState<Array<ITask>>([]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    setTask(event.target.value);
+  };
+
+  const addTask = (): void => {
+    const newTask = { taskName: task };
+    setTodoList([...todoList, newTask]);
+    setTask('');
+  };
+
+  const completeTask = (taskNameToDelete: string): void => {
+    setTodoList(
+      todoList.filter((task) => {
+        return task.taskName !== taskNameToDelete;
+      })
+    );
+  };
+
+  return (
+    <div className="App">
+      <div className="header">
+        <div className="inputContainer">
+          <input
+            type="text"
+            placeholder="Task..."
+            name="task"
+            value={task}
+            onChange={handleChange}
+          />
+        </div>
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <div className="todoList">
+        {todoList.map((task: ITask, key: number) => {
+          return <TodoTask key={key} task={task} completeTask={completeTask} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default App;
